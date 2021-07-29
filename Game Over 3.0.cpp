acb9bfbc98b0a95b823d314a152e5145fff5cca2
Sukhin <VVS11101992@gmail.com>
2021-07-29 18:55:39 +0300

    Add Game Over 3.0.cpp

---
+++ Game Over 3.0.cpp
@@ -0,0 +1,12 @@
+﻿// Программа Game Over 3.0
+// Демонстрирует работу с директивой using
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+  int main()
+ {
+      cout << "Game Over!" << endl;
+    return 0;
+ }
